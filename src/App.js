c3896083a828b59d9e8d8b7ae424b8ec427c6cd1
omar11developer <omarmenjivar083@gmail.com>
2022-10-09 22:39:49 -0600

    Creando Custom Hook para manejar localStorage

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@
 import { useState } from "react";
 
 //import img
-import NotFoundImg from './img/notFound.svg'
+import NotFoundImg from "./img/notFound.svg";
 
 //Data Array
 
 import AppUI from "./components/AppUI";
 
-
-function App() {
+function useLocalStorage(itemName, initialValue) {
   //PERSITENCIA OF DATA
   
-  const localStorageTodos = localStorage.getItem('TASK_V1');
-  let parsedTodos;
-  if (!localStorageTodos) {
-    localStorage.setItem('TASK_V1',JSON.stringify([]));
-    parsedTodos=[];
+  const localStorageItem= localStorage.getItem(itemName);
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   } else {
-    parsedTodos=JSON.parse(localStorageTodos);
+    parsedItem = JSON.parse(localStorageItem);
   }
-  const saveTodos= (newTodos) => {
+  
+  const [item, setItem] = useState(parsedItem);
+  
+  const saveItem = (newTodos) => {
     const stringiffiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TASK_V1', stringiffiedTodos);
-    setTodos(newTodos);
-  }
-  const [search, setSearch] = useState('');
-  const [todos, setTodos] = useState(parsedTodos);
+    localStorage.setItem(itemName, stringiffiedTodos);
+    setItem(newTodos);
+  };
+
+  return [
+    item,
+    saveItem,
+  ];
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TASK_V1', []);
+
+
+  const [search, setSearch] = useState("");
 
   const todoCompleted = todos.filter((todo) => !!todo.completed).length;
   const todoFavorite = todos.filter((todo) => !!todo.favorite).length;
@@ -37,7 +49,7 @@ function App() {
   if (!search.length >= 1) {
     searchedTodos = todos;
   } else {
-    searchedTodos = todos.filter(todo => {
+    searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = search.toLowerCase();
       const result = todoText.includes(searchText);
@@ -46,36 +58,36 @@ function App() {
   }
 
   const completeTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos= [...todos];
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
-  }
+  };
   const favoriteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos= [...todos];
-    newTodos[todoIndex].favorite=true;
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].favorite = true;
     saveTodos(newTodos);
-  }
+  };
   const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
-  }
-  
+  };
+
   return (
     <AppUI
-    totalTodos={totalTodos}
-    todoCompleted={todoCompleted}
-    search={search}
-    setSearch={setSearch}
-    searchedTodos={searchedTodos}
-    todoFavorite={todoFavorite}
-    NotFoundImg={NotFoundImg}
-    completeTodo={completeTodo}
-    favoriteTodo={favoriteTodo}
-    deleteTodo={deleteTodo}
+      totalTodos={totalTodos}
+      todoCompleted={todoCompleted}
+      search={search}
+      setSearch={setSearch}
+      searchedTodos={searchedTodos}
+      todoFavorite={todoFavorite}
+      NotFoundImg={NotFoundImg}
+      completeTodo={completeTodo}
+      favoriteTodo={favoriteTodo}
+      deleteTodo={deleteTodo}
     />
   );
 }
